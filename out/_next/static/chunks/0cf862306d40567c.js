6ba7d89b39d7ccec9eb5926e47828b3c2a06ea96
MuhammadRoshani <muhammadroshani79@gmail.com>
2025-12-26 15:31:51 +0330

    Add out folder for GitHub Pages

---
+++ out/_next/static/chunks/0cf862306d40567c.js
@@ -0,0 +1,7 @@
+__turbopack_load_page_chunks__("/products/[category]", [
+  "static/chunks/09161577f32fca2b.js",
+  "static/chunks/7ae2ef900959ee61.js",
+  "static/chunks/07eb9a0c441abc74.js",
+  "static/chunks/31531243d02c3de2.css",
+  "static/chunks/turbopack-3ae7a351f4abee5e.js"
+])
